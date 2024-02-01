e1b1b7f62b608dc7cf969ab325293daf7556f838
pathum <ckpathumck@gmail.com>
2024-02-01 15:20:37 +0530

    Doing basic ui

---
+++ src/main/java/lk/playtech/controller/ClientController.java
@@ -0,0 +1,26 @@
+package lk.playtech.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
+public class ClientController {
+    @FXML
+    private JFXButton btnSend;
+
+    @FXML
+    private ImageView imjSend;
+
+    @FXML
+    private ScrollPane scrollPane;
+
+    @FXML
+    private TextField txtMessege;
+
+    @FXML
+    private VBox vBox;
+
+}
---
+++ src/main/java/lk/playtech/controller/LoginController.java
@@ -0,0 +1,19 @@
+package lk.playtech.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class LoginController {
+    @FXML
+    private JFXButton btnJoin;
+
+    @FXML
+    private JFXTextField txtUserName;
+
+    @FXML
+    void btnJoinOnAction(ActionEvent event) {
+
+    }
+}
